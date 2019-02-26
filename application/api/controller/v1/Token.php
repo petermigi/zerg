21f443e44298ceac091d9a8309f971fa5080c424
peterliu <3102415881@qq.com>
2019-02-26 21:59:34 +0800

    14-3 应用令牌获取接口与应用令牌的理解

--- application/api/controller/v1/Token.php
+++ application/api/controller/v1/Token.php
@@ -3,7 +3,9 @@
 namespace app\api\controller\v1;
 
 use app\api\validate\TokenGet;
+use app\api\validate\AppTokenGet;
 use app\api\service\UserToken;
+use app\api\service\AppToken;
 use app\lib\exception\ParameterException;
 use app\api\service\Token as TokenService;
 
@@ -19,7 +21,25 @@ class Token
         return [
             'token' => $token
         ];
-    }   
+    }
+    
+    /**
+     * 第三方应用获取令牌
+     * @url /app_token?
+     * @POST ac=:ac se=:secret
+     */
+    public function getAppToken($ac='', $se='')
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: GET');
+        (new AppTokenGet())->goCheck();
+        $app = new AppToken();
+        $token = $app->get($ac, $se);
+        return [
+            'token' => $token
+        ];
+    }
 
     public function verifyToken($token='')
     {
--- application/route.php
+++ application/route.php
@@ -10,16 +10,18 @@
 // +----------------------------------------------------------------------
 
 use think\Route;
+
+//Banner
 //获取banner信息接口访问url路由
 Route::get('api/:version/banner/:id','api/:version.Banner/getBanner');
 
+//Theme
 //获取专题列表接口访问url路由
 Route::get('api/:version/theme/','api/:version.Theme/getSimpleList');
-
 //获取专题详情页信息接口访问url路由
 Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
 
-
+//Category
 //获取分类商品信息接口访问url路由
 Route::get('api/:version/product/by_category','api/:version.Product/getAllInCategory');
 //获取商品详情信息接口访问url路由
@@ -39,16 +41,21 @@ Route::get('api/:version/product/recent','api/:version.Product/getRecent');
 //获取分类信息接口访问url路由
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 
+//Token
 //获取令牌token接口访问url路由
 Route::post('api/:version/token/user','api/:version.Token/getToken');
 //令牌token有效性验证接口访问url路由
 Route::post('api/:version/token/verify','api/:version.Token/verifyToken');
+//CMS端令牌获取
+Route::post('api/:version/token/app', 'api/:version.Token/getAppToken');
 
+//Address
 //用户收货地址添加修改接口访问url路由
 Route::post('api/:version/address','api/:version.Address/createOrUpdateAddress');
 //获取用户收货地址接口访问url路由
 Route::get('api/:version/address','api/:version.Address/getUserAddress');
 
+//Order
 //订单接口访问url路由
 Route::post('api/:version/order','api/:version.Order/placeOrder');
 //历史订单接口访问url路由
@@ -56,6 +63,7 @@ Route::get('api/:version/order/by_user','api/:version.Order/getSummaryByUser');
 //订单详情接口访问url路由
 Route::get('api/:version/order/:id','api/:version.Order/getDetail',[],['id'=>'\d+']);
 
+//Pay
 //支付接口访问路由
 Route::post('api/:version/pay/pre_order','api/:version.Pay/getPreOrder');
 //微信支付回调接口访问路由
